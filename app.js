ec4b3f29c1af206c972adf32f59e3610e493b9f5
aniljogireddy <anil.jogireddy@gmail.com>
2021-11-20 17:59:56 +0100

    signup done

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cocktail',cocktailRouter)
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
