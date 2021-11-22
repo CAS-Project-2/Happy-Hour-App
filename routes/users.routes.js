99853cb18245b9c19c6f8476e0e4bbfd095bb1e6
aniljogireddy <anil.jogireddy@gmail.com>
2021-11-22 11:46:31 +0100

    editAndLogout

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,76 +1,152 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require("bcrypt")
 
 // to get user model
 const User = require("../models/User.model")
 
-const bcrypt = require('bcrypt');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 // routes for signup or register
 router.route('/signup')
-
-.get((req, res, next) => {
-  res.render('signup')
+.get((req, res) => {
+  res.render('signup');
 })
+.post( async (req, res)=>{
+const {username, email, password, favcocktail} = req.body
+  if(!username || !email || !password){
+    res.render("signup", { username, email, error:{type: "CRED_ERR", msg: "Missing credentials"}})
+  }
 
-.post((req, res)=>{
-  const {username,email,password} = req.body
-  if(!username || !email || !password) res.render("signup",{errorMessage:"Invalid username or password or email"})
-  User.findOne({username})
+  const user = await User.findOne({email})
+  if(user){
+    res.render("signup", { username, email, error:{type: "USR_ERR", msg: "Email exists"}})
+  }
 
-  .then((user)=>{
-    //to check if user already exists
-    if(user)res.render("signup",{errorMessage:"user exists, try to log in"})
-    const salt = bcrypt.genSaltSync(3)
-    const hashPassword = bcrypt.hashSync(password, salt)
+  const salt = bcrypt.genSaltSync(5)
+  const hashPwd = bcrypt.hashSync(password, salt)
 
-    //to crate the users if everything is find
+  await User.create({username, email, password: hashPwd, favcocktail})
+  res.redirect("/users/welcome-page")
 
-    User.create({username, password:hashPassword})
-    .then((req, res)=>{
-      res.render("index")
-    })
-    .catch((error)=>{
-      res.render("signup", {errorMessage:"Sorry for the inconvenience. Try again later."})
-    })
-  })
+
 })
-
 //login page now!!
-router.route("/login")
+ router.route("/login")
 
-.get((req, res, next)=>{
-  res.render("login")
-})
+  .get((req, res, next)=>{
+    res.render("login")
+  })
 
-.post((req, res)=>{
-  // thinking to giv more options to user. user can use mail or username to login const usernameOrMail = username || email
+
+.post( async (req, res)=>{
   const {username, password} = req.body
+  if(!username || !password){res.render("login", {error:{type: "CRED_ERR", msg: "Missing credentials"}})}
 
-  if(!username || !password) res.render("login", {errorMessage: "Username or password are required"})
-  User.findOne({username})
+  const loggedInUser = await User.findOne({username})
+  if(!loggedInUser) {res.render("login", {error:{type: "USR_ERR", msg: "User does not exist"}})}
+  
+  const pwsIsCorrect = bcrypt.compareSync(password, loggedInUser.password)
 
-  .then((user)=>{
-
-    if(username) res.render("login", {errorMessage:"User does not exist"})
-     // to check if the first pwd is matching the one encrypted from db
-    const passwordCorrect = bcrypt.compareSync(password, user.password) 
-
-    if(passwordCorrect)res.render("/index")
-
-    else res.render("login", {errorMessage:"password is Wrong"})
-  })
-  .catch((error)=>{
-    res.render("login", {errorMessage:"Sorry for the inconvenience. Try again later."})
-  })
+  if(pwsIsCorrect){
+    req.session.loggedInUser = loggedInUser
+    res.redirect("/users/welcome-page")
+  }else{
+    res.render(res.render("login", {error:{type: "PWD_ERR", msg: "Password incorrect"}}))
+  }
     
+}) 
+//logout
+router.get('/logout', (req, res) => {
+	req.session.destroy((err) => {
+		if (err) res.redirect('/');
+		else res.redirect('/users/login');
+	});
+});
+
+//profile
+router.route("/profile")
+.get((req, res)=>{
+  res.render('profile')
 })
+
+//welcome page
+router.route("/welcome-page")
+.get((req, res)=>{
+  res.render('welcome-page')
+})
+
+// edit profile
+router.route("/edit-profile")
+.get((req, res)=>{
+  res.render('edit-profile')
+})
+
+//edit Username
+router
+  .route("/:id/edit")
+  .get(async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findById(id);
+      const allUsers = await User.find();
+      const filteredUsers = allUsers.filter((cel) => {
+        return !users.find((cas) => cel.name === cas.name);
+      });
+
+      res.render("users/edit-profile", { user, allUsers: filteredUsers });
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const { id } = req.params;
+      const {username, favorites} = req.body;
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        { username, favorites },
+        { new: true }
+      );
+      res.redirect(`/users/${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
+
+//find user
+
+//edit user name and update them
+
+//find my cocktail
+
+// edit my cocktail and update them
+
+//find my cocktail
+
+//delete them
 
 
 
 module.exports = router;
+/* router
+  .route("/:id/edit")
+  .get(async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findById(id).populate('user');
+     // const allCelebs = await Celebrity.find();
+      const filteredUser = user.filter((cel) => {
+        return !users.user.find((cas) => cel.name === cas.name);
+      });
+
+      res.render("profile/username", { user, user: filteredUser });
+    } catch (error) {
+      console.log(error);
+    }
+  }) */
